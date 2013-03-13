480ee84bc0b0fd7c57ff851303ee0e3b6b56d348
Seamus O'Keefe <okeefe77@gmail.com>
2013-03-12 20:35:10 -0400

    finished plugin for specific case, created demo/text page

--- js/plugin.capHeight.js
+++ js/plugin.capHeight.js
@@ -3,13 +3,14 @@
         var w = (font_size * 0.8) | 0,
             h = font_size + 6;
 
-        var $canvas = $("<canvas width='" + w + "' height='" + h + "'></canvas>"),
+        var $canvas = $("<canvas width='" + w + "' height='" + h + "'>&nbsp;</canvas>"),
             canvas_css = {
                 position: 'absolute',
-                left: -9000
+                left: '-9000px'
             };
 
-        $('body').append($canvas).attr('id', 'testCanvas').css(canvas_css);
+        $canvas.attr('id', 'testCanvas').css(canvas_css);
+        $('body').append($canvas);
         
         return document.getElementById('testCanvas');
     };
@@ -22,7 +23,7 @@
             font = options.font_size + "px '" + family + "'",
             glyph_top = 0;
 
-        ctx.font(font);
+        ctx.font = font;
         ctx.fillText('E', 2, baseline);
 
         var img = ctx.getImageData(0, 0, cw, ch),
@@ -47,4 +48,26 @@
 
         return baseline - glyph_top;
     };
+
+    $.fn.CapHeight = function(options) {
+        options = $.extend({}, $.fn.CapHeight.defaults, options);
+        var canvas = createTestCanvas(options.font_size),
+            ctx = canvas.getContext('2d');
+        this.each(function() {
+            var $this = $(this);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            var glyph_height = findCapHeight(canvas, 
+                                             $this.css('font-family'),
+                                             options);
+            var line_height = glyph_height / options.font_size;
+            $this.css('line-height', line_height);
+        });
+    };
+
+    $.fn.CapHeight.defaults = {
+        font_size: 48,
+        match_threshold: 8,
+        alpha_threshold: 245
+    };
+
 })(jQuery);
